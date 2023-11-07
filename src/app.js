93d1be95bdfbeb176c8fefff465d00daf344698d
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-11-07 11:28:44 +0000

    part 2 and 3

--- src/app.js
+++ src/app.js
@@ -8,4 +8,32 @@ app.get("/restaurants", async (req, res) => {
   res.json(restaurants);
 });
 
+app.get("/restaurants/:id", async (req, res) => {
+  const number = req.params.id;
+  const restaurant = await Restaurant.findByPk(number);
+  res.json(restaurant);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/restaurants", async (req, res) => {
+  const restaurant = await Restaurant.create(req.body);
+  res.json(restaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const updatedRest = await Restaurant.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updatedRest);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const deletedRest = await Restaurant.destroy({
+    where: { id: req.params.id },
+  });
+  res.json(deletedRest);
+});
+
 module.exports = app;
